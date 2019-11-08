40cd8f645f3ef23843f827be2b019f3cf572c2f1
马鹏飞 <merrily@live.com>
2019-11-08 13:22:24 +0800

    create source files for gauss quadrature.

---
+++ GaussQuadrature.h
@@ -0,0 +1,16 @@
+#include <vector>
+
+
+/// Compute quadrature rule for triangle.
+///
+/// *Arguments*
+///     coordinates (std::vector<Point>)
+///         Vertex coordinates for the triangle
+///
+/// *Returns*
+///     std::pair<std::vector<double>, std::vector<double>>
+///         A flattened array of quadrature points and a
+///         corresponding array of quadrature weights.
+std::pair<std::vector<double>, std::vector<double>> quadrature_weights_nodes() {
+	
+}
