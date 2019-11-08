c417ef3cdd6f1a73389445d43c2d92eca6de55f1
马鹏飞 <merrily@live.com>
2019-11-08 13:36:53 +0800

    create files for general elements collection

---
+++ GeneralElementsCollection.h
@@ -0,0 +1,11 @@
+#pragma once
+template<std::size_t N, std::size_t M>
+class GeneralElementsCollection {
+public:
+	const std::size_t geometric_dimension() { return N; }
+	const std::size_t fenitespace_dimension() { return M; }
+private:
+	std::vector<std::array<double, N>> _p;
+	std::vector<std::array<std::size_t, M>> _T;
+
+};
