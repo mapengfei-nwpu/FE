38599d9a93bfd0bf81379645ee3ed913fb15cba2
马鹏飞 <merrily@live.com>
2019-11-08 16:29:23 +0800

    write comments for GenralElementsCollection.h

--- GeneralElementsCollection.h
+++ GeneralElementsCollection.h
@@ -4,12 +4,47 @@
 template<std::size_t N, std::size_t M>
 class GeneralElementsCollection {
 public:
+
+	/// return geometric dimesnion. it decides how many
+	/// axis every node has.
 	const std::size_t geometric_dimension() { return N; }
-	const std::size_t finitespace_dimension() { return M; }
-	void add_node(std::array<double, N> node) { _P.push_back(node); }
-	std::array<double,N> get_node(double i) { return _P[i]; };
+
+	/// return element dimension which is the number of
+	/// nodes every element contains.
+	const std::size_t element_dimension() { return M; }
+
+
+	/// Get a node
+	///
+	/// *Arguments*
+	///     i (std::size_t)
+	///         the index of the node you want to get.
+	std::array<double, N> get_node(std::size_t i) { return _P[i]; };
+
+	/// Get an element.
+	///
+	/// *Arguments*
+	///     i (std::size_t)
+	///         the index of the element you want to get.
 	std::array<std::size_t, M> get_element(std::size_t i) { return _T[i]; };
+
+	/// add a node to nodes collection.
+	///
+	/// *Arguments*
+	///     node (std::array<double, N>)
+	void add_node(std::array<double, N> node) { _P.push_back(node); }
+
+	/// add an element to the tabulation. Here, "element" means the
+	/// connectivity or tabulation of an element, which contians indices
+	/// of all nodes on the element. no matter mesh nodes or dof nodes,
+	/// finite element or mesh element.
+	///
+	/// *Arguments*
+	///     element (std::array<std::size_t, M>)
+	///         The tabulation of which the indices of the element consist
 	void add_element(std::array<std::size_t, M> element) { _T.push_back(element); }
+
+	/// Print the points and connectivity of the mesh.
 	void print() {
 		std::cout << "Points:" << std::endl;
 		for (std::size_t i = 0; i < _P.size(); ++i) {
@@ -20,7 +55,12 @@ public:
 			std::cout << _T[i][0] << "," << _T[i][1] << "," << _T[i][2] << std::endl;
 		}
 	}
+
 private:
+	/// The collection which contains the coordinates of all points.
 	std::vector<std::array<double, N>> _P;
+
+	/// The tabulation for every element.
 	std::vector<std::array<std::size_t, M>> _T;
+
 };
