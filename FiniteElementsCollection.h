e3dc0f51b708768960639be98b456bbc710eaa3d
马鹏飞 <merrily@live.com>
2019-11-09 22:57:06 +0800

    Update FiniteElementsCollection.h

--- FiniteElementsCollection.h
+++ FiniteElementsCollection.h
@@ -1,12 +1,30 @@
 #pragma once
-#include "GeneralElementsCollection.h"
-#include "FiniteElement.h"
 #include <array>
 #include <vector>
+#include <memory>
 #include <boost/multi_array.hpp>
+
+#include "FiniteElement.h"
+#include "MeshElementsCollection.h"
+
+/// I found that GeneralElementCollection cannot use template.
 class FiniteElementsCollection :public GeneralElementsCollection<2, 3> {
 public:
+	FiniteElementsCollection(MeshElementsCollection mec);
+	std::size_t element_dim();
+	std::size_t element_number();
+	std::size_t dof_number() { return element_dim() * element_number(); };
+	FiniteElement get_Finite_element(std::size_t i);
 
-	// FiniteElement get_finite_element(std::size_t i);
+private:
+	// The mesh
+	MeshElementsCollection *_mesh;
+
+	// Topological dimension
+	std::size_t _dim;
+
+	// Local index of entity within topological dimension
+	std::size_t _local_index;
+
 };
 
