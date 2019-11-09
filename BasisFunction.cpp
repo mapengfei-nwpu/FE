5e7ac40507c0b02e0cea6016fd1b1113d429bb88
mapengfei <mapengfei@yahoo.com>
2019-11-09 13:50:07 +0800

    write basis functions
    1st order basis functions and their derivatives for triangle in 2D space.

--- BasisFunction.cpp
+++ BasisFunction.cpp
@@ -3,19 +3,6 @@
 
 #include "BasisFunction.h"
 
-std::function<double(double, double)> phi_x;
-auto mod = [](int a, int b) {return a % b;};
-void compute_jacobian_determinants() { ; }
-
-auto psi = [](int a) {return a; };
-auto psi_derivative_x  = [](int a) {return a; };
-auto psi_derivative_y  = [](int a) {return a; };
-
-int mammin() {
-
-	return 0;
-}
-
 
 
 
--- BasisFunction.h
+++ BasisFunction.h
@@ -2,5 +2,65 @@
 
 #include "MeshElement.h"
 class BasisFunction {
-	BasisFunction(MeshElement e);
+public:
+	BasisFunction(const MeshElement& e) {
+		auto c = e.coordinates;
+		x1 = c[0][0]; y1 = c[0][1];
+		x2 = c[1][0]; y2 = c[1][1];
+		x3 = c[2][0]; y3 = c[2][1];
+		_j = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1);
+
+
+	}
+	double get_jacobian_determian() {
+		return _j;
+	}
+
+	double basis_1(double x, double y) {
+		return -x_reference(x, y) - y_reference(x, y) + 1;
+	}
+	double basis_1_derivative_x(double x, double y) {
+		return -(y3 - y1) / _j - (y1 - y2) / _j;
+	}
+	double basis_1_derivative_y(double x, double y) {
+		return -(x1 - x3) / _j - (x2 - x1) / _j;
+	}
+
+	double basis_2(double x, double y) {
+		return x_reference(x, y);
+	}
+	double basis_2_derivative_x(double x, double y) {
+		return (y3 - y1) / _j;
+	}
+	double basis_2_derivative_y(double x, double y) {
+		return (x1 - x3) / _j;
+	}
+
+	double basis_3(double x, double y) {
+		auto z = y_reference(x, y);
+		return z;
+	}
+	double basis_3_derivative_x(double x, double y) {
+		return (y1 - y2) / _j;
+	}
+	double basis_3_derivative_y(double x, double y) {
+		return (x2 - x1) / _j;
+	}
+
+
+private:
+
+	// the determinant of jacobian matrix.
+	double _j;
+	// coordinates
+	double x1, x2, x3, y1, y2, y3;
+
+	// reference coordinates.
+	double x_reference(double x, double y) {
+		return ((y3 - y1) * (x - x1) - (x3 - x1) * (y - y1)) / _j;
+	};
+	double y_reference(double x, double y) {
+		return (-(y2 - y1) * (x - x1) + (x2 - x1) * (y - y1)) / _j;
+	};
+
 };
