737ae9ffa0728eeb8453350972c84c63424e589e
马鹏飞 <merrily@live.com>
2019-11-12 09:45:19 +0800

    Update FiniteElementsCollection.h
    finish the _T part.

--- FiniteElementsCollection.h
+++ FiniteElementsCollection.h
@@ -9,54 +9,63 @@
 #include "FiniteElement.h"
 #include "MeshElementsCollection.h"
 
+class Edge {
+public:
+	Edge(size_t start, size_t end) : start(start), end(end) {
+		if (end < start) { std::swap(end, start); }
+	}
+	size_t start;
+	size_t end;
+};
+
 class FiniteElementsCollection :public GeneralElementsCollection<2, 6> {
 public:
 	/// construct from mesh and order.
-	FiniteElementsCollection(MeshElementsCollection mec, std::size_t order = 1) {
-		if (order == 2) {
-			/// generate the index of finite element nodes.
-			std::size_t index = mec._P.size();
-			std::map<std::size_t, std::map<std::size_t, std::size_t>> midpoint;
-			for (size_t i = 0; i < mec._T.size(); i++)
-			{
-				_T[i][_T[0].size() - 1], _T[i][0];
-				for (size_t j = 1; j < _T[0].size(); j++)
-				{
-					std::size_t start, end;
-					if (_T[i][j - 1] > _T[i][j]) {
-						start = _T[i][j];
-						end = _T[i][j - 1];
-					}
-					else {
-						end = _T[i][j];
-						start = _T[i][j - 1];
-					}
-					midpoint[start][end] = index++;
-				}
-			}
-			/// generate new _T;
-			for (size_t i = 0; i < mec._T.size(); i++)
-			{
-				std::array<std::size_t, 6> t;
-				std::size_t start, end;
-				t[0] = mec._T[i][0];
-				/// start = 
-				/// end   =
-				t[1] = midpoint[start][end];
-				t[2] = mec._T[i][1];
-				/// start = 
-				/// end   =
-				t[3] = midpoint[start][end];
-				t[4] = mec._T[i][2];
-				/// start = 
-				/// end   =
-				t[5] = midpoint[start][end];
-				_T.push_back(t);
-			}
-		}
-		else {
+	FiniteElementsCollection(MeshElementsCollection mesh, size_t order = 2) {
+		if (order != 2) {
 			std::cout << "Finite element order must be one." << std::endl;
 		}
+
+		/// generate index for midpoints on every edge.
+		size_t mesh_element_size = mesh._T[0].size();
+		size_t mesh_point_number = mesh._P.size();
+		std::map<size_t, std::map<size_t, size_t>> midpoint;
+		for (size_t i = 0; i < mesh._T.size(); i++)
+		{
+			Edge edge(mesh._T[i][mesh_element_size - 1], mesh._T[i][0]);
+			midpoint[edge.start][edge.end] = mesh_point_number++;
+			for (size_t j = 1; j < mesh_element_size; j++)
+			{
+				Edge edge(mesh._T[i][j - 1], mesh._T[i][j]);
+				midpoint[edge.start][edge.end] = mesh_point_number++;
+			}
+		}
+		
+		/// generate new _T which is the tabulation of finite elements.
+		for (size_t i = 0; i < mesh._T.size(); i++)
+		{
+			std::array<size_t, 6> t;
+			size_t start, end;
+			t[0] = mesh._T[i][0];
+			Edge edge01(mesh._T[i][0], mesh._T[i][1]);
+			t[1] = midpoint[start][end];
+			t[2] = mesh._T[i][1];
+			Edge edge12(mesh._T[i][1], mesh._T[i][2]);
+			t[3] = midpoint[start][end];
+			t[4] = mesh._T[i][2];
+			Edge edge20(mesh._T[i][2], mesh._T[i][0]);
+			t[5] = midpoint[start][end];
+			_T.push_back(t);
+		}
+
+		/// generate new _P for the finite elements
+		rsize_t finite_point_number = mesh_point_number + midpoint.size();
+		_P.resize(finite_point_number);
+		for (size_t i = 0; i < mesh_point_number; i++)
+		{
+			_P[i] = mesh._P[i];
+		}
+		
+
 	}
-private:
 };
