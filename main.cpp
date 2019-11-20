f7ba2422b6bf38ea09f20b900ec458c64703ac17
mapengfei-nwpu <mapengfei@yahoo.com>
2019-11-20 18:32:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,12 @@ using namespace dolfin;
 
 class xplusy : public Expression
 {
+public:
+	xplusy() : Expression(2) { }
 	void eval(Array<double>& values, const Array<double>& x) const
 	{
-		values[0] = 2.0 + x[0] + x[1];
+		values[0] = x[0];
+		values[1] = x[1];
 	}
 };
 
@@ -22,13 +25,17 @@ int main()
 	auto g = std::make_shared<xplusy>();
 	Function u(V);
 	u.interpolate(*g);
-	std::vector<double> body(4);
+	std::vector<double> body(8);
 	std::vector<std::array<double, 2>> body_coordinates(4);
-	body_coordinates[0][0] = 0.25; body_coordinates[0][1] = 0.25;
-	body_coordinates[1][0] = 0.25; body_coordinates[1][1] = 0.75;
-	body_coordinates[2][0] = 0.75; body_coordinates[2][1] = 0.25;
-	body_coordinates[3][0] = 0.75; body_coordinates[3][1] = 0.75;
+	body_coordinates[0][0] = 0.1; body_coordinates[0][1] = 0.5;
+	body_coordinates[1][0] = 0.2; body_coordinates[1][1] = 0.6;
+	body_coordinates[2][0] = 0.3; body_coordinates[2][1] = 0.7;
+	body_coordinates[3][0] = 0.4; body_coordinates[3][1] = 0.8;
 	DeltaInterplation::fun1(u, ba, body, body_coordinates);
+	for (size_t i = 0; i < 8; i++)
+	{
+		std::cout << body[i] << std::endl;
+	}
 
 
 	File file("xplusy.pvd");
